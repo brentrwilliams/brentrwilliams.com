6c965ca305142e5be9b0a41d2f05e717c269911e
Brent Williams <brent.robert.williams@gmail.com>
2015-06-26 09:43:19 -0700

    Finished implementing initial design

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+$(document).ready ( function(){
+   
+});
+
+$('#aboutLink').on('click',function() {
+   $('#resume').hide();
+   $('#projects').hide();
+   $('#contact').hide();
+   $('#about').show();
+   return false; // Makes it so the anchor doesn't reload the page
+});
+
+$('#projectsLink').on('click',function() {
+   $('#about').hide();
+   $('#contact').hide();
+   $('#resume').hide();
+   $('#projects').show();
+   return false; // Makes it so the anchor doesn't reload the page
+});
+
+$('#resumeLink').on('click',function() {
+   $('#about').hide();
+   $('#projects').hide();
+   $('#contact').hide();
+   $('#resume').show();
+   return false; // Makes it so the anchor doesn't reload the page
+});
+
+$('#contactLink').on('click',function() {
+   $('#about').hide();
+   $('#projects').hide();
+   $('#resume').hide();
+   $('#contact').show();
+   return false; // Makes it so the anchor doesn't reload the page
+});
